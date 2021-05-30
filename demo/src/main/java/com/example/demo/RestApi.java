311e03ae129a87ce446f97e373225ee4e248cbec
ritresh <ritresh.girdhar@loblaw.ca>
2021-05-30 14:31:18 +0530

    API changes

--- demo/src/main/java/com/example/demo/RestApi.java
+++ demo/src/main/java/com/example/demo/RestApi.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class RestApi {
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot and Jib!";
+	}
+
+	@RequestMapping(value = "/hostname",method = RequestMethod.GET)
+	public String getHostName() throws UnknownHostException {
+		return "Welcome to" + InetAddress.getLocalHost().getHostName();
 	}
 }
